9f4d1c7ac52dffeb4e5cce60519c7227a8260a10
Малинин Александр Юрьевич <malininayu@sibur.ru>
2022-02-03 23:35:23 +0300

    calendar data generation

--- TODO-List/TODO-List/Modules/MainEventsListModule/CalendarSubModule/CalendarPresenter.swift
+++ TODO-List/TODO-List/Modules/MainEventsListModule/CalendarSubModule/CalendarPresenter.swift
@@ -21,11 +21,64 @@ class CalendarPresenter: CalendarPresenterProtocol {
         self.router = router
     }
     
+    //MARK: TODO - change selectedDate!
+    private let selectedDate = Date()
+    
+    
+    private let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d"
+        return dateFormatter
+    }()
+    
+    
     func createMonthMetaData(for baseDate: Date) throws -> MonthMetaData {
         guard let numberOfDaysInMonth = view?.calendar.range(of: .day, in: .month, for: baseDate)?.count, let dateComponents = view?.calendar.dateComponents([.year, .month], from: baseDate), let firstDayOfMonth = view?.calendar.date(from: (dateComponents)),  let firstDayOfWeek = view?.calendar.component(.weekday, from: firstDayOfMonth) else {
             throw CalendarError.monthMetaDataGeneration}
         
-        return MonthMetaData(numberOfDays: numberOfDaysInMonth, monthFirstDay: firstDayOfMonth, weekFirstDay: firstDayOfWeek)
+        return MonthMetaData(numberOfDays: numberOfDaysInMonth, monthFirstDate: firstDayOfMonth, weekFirstDayNumber: firstDayOfWeek)
+    }
+    
+    func generateDaysInMonth(for baseDate: Date) -> [Day] {
+        guard let monthMetaData = try? createMonthMetaData(for: baseDate) else {
+            fatalError(CalendarError.monthMetaDataGeneration.rawValue)
+        }
+            
+            let numberOfDaysInMonth = monthMetaData.numberOfDays
+            let offsetInInitialRow = monthMetaData.weekFirstDayNumber
+            let firstDateOfMonth = monthMetaData.monthFirstDate
+            
+            let days: [Day] = (1..<(numberOfDaysInMonth + offsetInInitialRow)).map{day in
+                let isWithinDisplayedMonth = day >= offsetInInitialRow
+                
+                let dayOffset = isWithinDisplayedMonth ? day - offsetInInitialRow : -(offsetInInitialRow - day)
+                
+                return generateDay(offsetBy: dayOffset, for: firstDateOfMonth, isWithinDisplayedMonth: isWithinDisplayedMonth)
+                
+            }
+        
+            return days
+    }
+    
+    
+    func generateDay(offsetBy dayOffset: Int, for baseDate: Date, isWithinDisplayedMonth: Bool) -> Day {
+        let date = view?.calendar.date(byAdding: .day, value: dayOffset, to: baseDate) ?? baseDate
+        
+        return Day(date: date, number: dateFormatter.string(from: date), isSelected: view?.calendar.isDate(date, inSameDayAs: selectedDate) ?? false, isWithinDisplayedMonth: isWithinDisplayedMonth)
+    }
+    
+    
+    func generateStartOfNextMonth(using firstDayOfDisplayedMonth: Date) -> [Day] {
+        guard let lastDayInMonth = view?.calendar.date(byAdding: DateComponents(month: 1, day: -1), to: firstDayOfDisplayedMonth), let lastDayInMonthDayNum = view?.calendar.component(.weekday, from: lastDayInMonth) else {return []}
+        
+        let additionalDays = 7 - lastDayInMonthDayNum
+        guard additionalDays > 0 else {return []}
+        
+        let days: [Day] = (1...additionalDays).map{day in
+            return generateDay(offsetBy: day, for: lastDayInMonth, isWithinDisplayedMonth: false)
+        }
+        
+        return days
     }
     
     
